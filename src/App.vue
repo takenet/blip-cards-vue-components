a5de0f104ac61325eadc8aafe4bb42c9d2b2c457
Breno Fernandes Queiroz <brenof@takenet.com.br>
2017-10-30 18:29:39 -0200

    plain text sample

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="width: 250px">
+    <PlainText :length="10" pos="right" :documents="[{content: 'Oi'}]"/>
+    <PlainText pos="left" :documents="[{content: 'Estou ótimo! Ao contrário do que se  acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos, suas raízes.'}]"/>
+  </div>
+</template>
+
+<script>
+import PlainText from './components/PlainText'
+
+export default {
+  components: {
+    PlainText
+  }
+}
+</script>
---
+++ src/components/PlainText.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div :class="'bubble plain-text ' + pos" v-for="(item, index) in previewDocuments">
+      <div v-if="!item.hasPreview">
+        {{ item.content }}
+      </div>
+      <div v-else>
+        <div v-show="!item.showContent">
+          {{ item.previewContent }}
+        </div>
+        <div v-show="item.showContent">
+          {{ item.content }}
+        </div>
+        <a v-show="!item.showContent" v-on:click="showContent(item)">Ver mais</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    length: {
+      type: Number,
+      default: 10
+    },
+    documents: {
+      type: Array,
+      required: true
+    },
+    pos: {
+      type: String,
+      default: 'left'
+    }
+  },
+  data: function () {
+    return {
+      previewDocuments: this.documents.filter(x => {
+        x.previewContent = x.content.substring(0, this.length)
+        x.hasPreview = x.content.length > this.length
+        x.showContent = false
+        return x
+      })}
+  },
+  methods: {
+    showContent: function (document) {
+      document.showContent = true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+   @import '../styles/common.scss';
+
+   a {
+     text-decoration: none;
+     color: blue;
+     display: block;
+     color: #0CC8CC;
+   }
+</style>
