d9ec734177a14600dd9bba2bbb54d46861b2974e
Breno Fernandes Queiroz <brenof@takenet.com.br>
2017-11-09 09:29:21 -0200

    fix(fix install modules): fix install modules

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
+import BlipCard from './components/BlipCard'
 import PlainText from './components/PlainText'
+import BlipSelect from './components/BlipSelect'
+import DocumentSelect from './components/DocumentSelect'
 import MediaLink from './components/MediaLink'
+import Collection from './components/Collection'
 
-function install(Vue) {
+function install (Vue) {
   let components = []
-  components.push(
-    { name: 'plainText', component: Vue.component('plain-text', PlainText) },
-    { name: 'mediaLink', component: Vue.component('media-link', MediaLink) }
-  )
+
+  components.push(Vue.component('blip-card', BlipCard))
+  components.push(Vue.component('plain-text', PlainText))
+  components.push(Vue.component('media-link', MediaLink))
+  components.push(Vue.component('blip-select', BlipSelect))
+  components.push(Vue.component('document-select', DocumentSelect))
+  components.push(Vue.component('collection', Collection))
+
   return components
 }
 
 export {
+  BlipCard,
   PlainText,
+  BlipSelect,
+  DocumentSelect,
   MediaLink,
+  Collection,
   install
 }
 
