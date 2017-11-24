17e9f3ae25bebd996d4404e6fb1d2b0b76f5480d
Andressa Leal Cruz <andressa@take.net>
2017-11-24 19:06:16 -0200

    feat(File card): Add edit option for card

--- src/components/MediaLink/BlipFile.vue
+++ src/components/MediaLink/BlipFile.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="blip-container file">
+  <div>
     <div :class="'bubble ' + position">
-      <a :href="this.uri" target="_blank" class="file-wrapper">
-        <div class="file-icon-wrapper">
-          <img class="file-icon" :src="type | fileIconFilter"/>
-        </div>
-        <div class="description-wrapper">
-          <div class="link-description">
-            <span v-if="this.title" :title="title" class="text big-text">{{ title }}</span>
-            <span v-else :title="uri" class="text big-text">{{ uri }}</span>
+      <div v-if="editable" class="editIco trashIco" @click="trash(document)">
+        <img :src="trashSvg" />
+      </div>
+      <div v-if="editable && !isEditing" class="editIco" @click="toggleEdit">
+        <img :src="editSvg" />
+      </div>
+      <div v-if="!isEditing">
+        <a :href="document.uri" target="_blank" class="file-wrapper">
+          <div class="file-icon-wrapper">
+            <img class="file-icon" :src="document.type | fileIconFilter"/>
           </div>
-          <span class="text small-text">{{ size | sizeInBytesFilter }}</span>
-        </div>
-      </a>
+          <div class="description-wrapper">
+            <div class="link-description">
+              <span v-if="document.title" :title="document.title" class="text big-text">{{ document.title }}</span>
+              <span v-else :title="document.uri" class="text big-text">{{ document.uri }}</span>
+            </div>
+            <span v-if="document.size" class="text small-text">{{ document.size | sizeInBytesFilter }}</span>
+          </div>
+        </a>
+      </div>
+      <div v-else>
+        <form novalidate v-on:submit.prevent>
+          <div class="saveIco closeIco" @click="fileCancel()" >
+            <img :src="closeSvg" />
+          </div>
+          <div class="saveIco" @click="fileSave()" :class="{'is-disabled': errors.any() }">
+            <img :src="approveSvg" />
+          </div>
+          <div class="form-group">
+            <input type="text" name="file" class="form-control uri" v-model="uri" placeholder="File URL" :class="{'input-error': errors.has('file') }" v-validate="'required|url'"/>
+            <span v-if="errors.has('file')" class="help input-error">{{ errors.first('file') }}</span>
+            <input type="text" class="form-control tittle" v-model="title" placeholder="Title"/>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +43,7 @@
 <script>
 
 import { default as base } from '../../mixins/baseComponent.js'
+import mime from 'mime-types'
 
 export default {
   mixins: [
@@ -27,11 +51,28 @@ export default {
   ],
   data: function () {
     return {
-      imgPreview: this.document.previewUri,
       title: this.document.title,
       uri: this.document.uri,
       type: this.document.type,
       size: this.document.size
+    }
+  },
+  methods: {
+    fileSave: async function () {
+      let result = await this.$validator.validateAll()
+      if (!result) return
+
+      this.save({
+        ...this.document,
+        title: this.title,
+        uri: this.uri,
+        type: mime.lookup(this.uri)
+      })
+    },
+    fileCancel: function () {
+      this.isEditing = false
+      this.title = this.document.title
+      this.uri = this.document.uri
     }
   }
 }
@@ -42,78 +83,77 @@ export default {
    @import '../../styles/variables.scss';
 
   .media-link.application {
-
     .bubble {
-      padding: 0;
-      height: 80px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      overflow: hidden;
-    }
-
-    .left {
-      .description-wrapper {
-        color: $vue-gray;
-      }
-    }
-
-    .right {
-      .file-icon-wrapper {
-        background-color: $vue-white;
-        padding-right: 20px !important;
-      }
-      .description-wrapper {
-        color: $vue-white;
-        padding-left: 20px !important;
-      }
-    }
-
-    .file-wrapper {
-      text-decoration: inherit;
-      display: flex;
-      flex-direction: row;
-      align-content: center;
-      justify-content: center;
-
-      .file-icon-wrapper {
-        display: flex;
-        padding: 20px;
-        padding-right: 10px;
-        justify-content: center;
-
-        .file-icon {
-          display: flex;
-          flex-direction: horizontal;
-          flex-grow: 1;
-          max-height: 40px;
-          max-width: 40px;
-          object-fit: contain;
+      &.left {
+        .description-wrapper {
+          color: $vue-gray;
         }
       }
 
-      .description-wrapper {
-        overflow: hidden;
-        padding: 20px;
-        padding-left: 10px;
-        display: flex;
-        flex-direction: column;
+      &.right {
+        .file-icon-wrapper {
+          background-color: $vue-white;
+          padding-right: 20px !important;
+        }
+        .description-wrapper {
+          color: $vue-white;
+          padding-left: 20px !important;
+        }
+      }
 
-        .link-description {
+      .file-wrapper {
+        padding: 0;
+        height: 80px;
+        text-decoration: inherit;
+        display: flex;
+        flex-direction: row;
+        align-content: center;
+        justify-content: center;
+
+        .file-icon-wrapper {
           display: flex;
-          flex-direction: column;
-          flex-grow: 1;
-          .text {
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
+          padding: 20px;
+          padding-right: 10px;
+
+          .file-icon {
+            display: flex;
+            flex-direction: horizontal;
+            flex-grow: 1;
+            max-height: 40px;
+            max-width: 40px;
+            object-fit: contain;
           }
         }
 
-        .small-text {
-          font-size: 10px;
-          font-weight: 100;
+        .description-wrapper {
+          overflow: hidden;
+          padding: 20px;
+          padding-left: 10px;
+          display: flex;
+          flex-direction: column;
+
+          .link-description {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            .text {
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+            }
+          }
+
+          .small-text {
+            font-size: 10px;
+            font-weight: 100;
+          }
         }
+      }
+    }
+
+    .form-group {
+      .form-control.tittle {
+        margin-top: 10px;
       }
     }
   }
