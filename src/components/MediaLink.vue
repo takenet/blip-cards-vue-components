4886a00d8c9109d9e5e862dcede756cc1308e931
Breno Queiroz <brenoqueiroz@gmail.com>
2017-11-27 13:19:17 -0200

    fix blip file ico

--- src/components/MediaLink.vue
+++ src/components/MediaLink.vue
@@ -2,9 +2,9 @@
   <div :class="'blip-container media-link ' + document.type.split('/')[0]">
 
     <blip-image :document="document" :position="position" :date="date" v-if="document.type.indexOf('image') != -1" :editable="editable" :on-save="save" :on-deleted="onDeleted"/>
-    <blip-audio :document="document" :position="position" :date="date" v-if="document.type.indexOf('audio') != -1" :editable="editable" :on-save="save" :on-deleted="onDeleted"/>
-    <blip-video :document="document" :position="position" :date="date" v-if="document.type.indexOf('video') != -1" :editable="editable" :on-save="save" :on-deleted="onDeleted"/>
-    <blip-file :document="document" :position="position" :date="date" v-if="document.type.indexOf('application') != -1" :editable="editable" :on-save="save" :on-deleted="onDeleted"/>
+    <blip-audio :document="document" :position="position" :date="date" v-else-if="document.type.indexOf('audio') != -1" :editable="editable" :on-save="save" :on-deleted="onDeleted"/>
+    <blip-video :document="document" :position="position" :date="date" v-else-if="document.type.indexOf('video') != -1" :editable="editable" :on-save="save" :on-deleted="onDeleted"/>
+    <blip-file :document="document" :position="position" :date="date" v-else :editable="editable" :on-save="save" :on-deleted="onDeleted"/>
 
     <div :class="'notification ' + position" v-if="date">
       {{ date }}
