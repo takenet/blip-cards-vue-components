ec984787b49ce19e6eed721305ba4e8df53376f5
Victor Balbo de Oliveira (Take.net) <victorb@take.net>
2019-01-08 14:58:20 -0200

    feat(All cards): Add class to identify messages with failed notification

--- src/components/ChatState.vue
+++ src/components/ChatState.vue
@@ -1,12 +1,16 @@
 <template>
   <div v-if="!isEditing">
-    <div v-if="this.document.state === 'composing'" class="blip-container chat-state" :class="isFailedMessage(status, position)">
+    <div
+      v-if="this.document.state === 'composing'"
+      class="blip-container chat-state"
+      :class="isFailedMessage(status, position)"
+    >
       <div :class="'bubble ' + position">
         <div v-if="deletable" class="editIco trashIco" @click="trash(document)">
-          <img :src="trashSvg" />
+          <img :src="trashSvg">
         </div>
         <div v-if="editable" class="editIco" @click="toggleEdit">
-          <img :src="editSvg" />
+          <img :src="editSvg">
         </div>
         <div class="typing">
           <div class="typing-dot"></div>
@@ -20,42 +24,51 @@
   <div class="blip-container" v-else>
     <div :class="'bubble ' + position">
       <form novalidate v-on:submit.prevent>
-        <button type="button" class="btn saveIco closeIco" @click="cancel()" >
-          <img :src="closeSvg" />
+        <button type="button" class="btn saveIco closeIco" @click="cancel()">
+          <img :src="closeSvg">
         </button>
         <button class="btn saveIco" @click="saveState()" :class="{'is-disabled': errors.any() }">
-          <img :src="approveSvg" />
+          <img :src="approveSvg">
         </button>
         <div class="form-group">
-          <input type="text" name="interval" class="form-control" v-validate="'required|numeric'"
-            :class="{'input-error': errors.has('interval') }" v-model="interval" placeholder="Milliseconds to wait"/>
-          <span v-show="errors.has('interval')" class="help input-error">{{ errors.first('interval') }}</span>
+          <input
+            type="text"
+            name="interval"
+            class="form-control"
+            v-validate="'required|numeric'"
+            :class="{'input-error': errors.has('interval') }"
+            v-model="interval"
+            placeholder="Milliseconds to wait"
+          >
+          <span
+            v-show="errors.has('interval')"
+            class="help input-error"
+          >{{ errors.first('interval') }}</span>
         </div>
-        <button v-if="typeof onMetadataEdit === 'function'" class="define-metadata blip-chat-state-metadata" @click="editMetadata(fullDocument)">
-          {{ metadataButtonText }}
-        </button>
+        <button
+          v-if="typeof onMetadataEdit === 'function'"
+          class="define-metadata blip-chat-state-metadata"
+          @click="editMetadata(fullDocument)"
+        >{{ metadataButtonText }}</button>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-
 import { default as base } from '../mixins/baseComponent.js'
 import { isFailedMessage } from '../utils/misc'
 
 export default {
   name: 'chat-state',
-  mixins: [
-    base
-  ],
+  mixins: [base],
   props: {
     status: {
       type: String,
       default: ''
     }
   },
-  data: function () {
+  data: function() {
     return {
       interval: undefined,
       isFailedMessage
@@ -63,9 +76,11 @@ export default {
   },
   methods: {
     init: function() {
-      this.interval = this.document.interval ? this.document.interval : undefined
+      this.interval = this.document.interval
+        ? this.document.interval
+        : undefined
     },
-    saveState: function () {
+    saveState: function() {
       this.save({
         ...this.document,
         interval: this.interval
@@ -77,51 +92,51 @@ export default {
 
 
 <style lang="scss" scoped>
-   @import '../styles/variables.scss';
+@import '../styles/variables.scss';
 
-  .chat-state .bubble {
-    padding: $bubble-padding;
-    word-wrap: break-word;
-    min-width: auto;
+.chat-state .bubble {
+  padding: $bubble-padding;
+  word-wrap: break-word;
+  min-width: auto;
 
-    .typing {
-      display: inline-block;
-    }
-    .typing .typing-dot {
-        float: left;
-        width: 6px;
-        height: 6px;
-        border-radius: 50%;
-        margin: 2px;
-        background: #929292;
-        animation-name: bounce;
-        animation-duration: 0.8s;
-        animation-iteration-count: infinite;
-        animation-timing-function: ease-in-out;
-    }
-
-    .typing .typing-dot:nth-child(2) {
-        animation-delay: 0.1s;
-    }
-    .typing .typing-dot:nth-child(3) {
-        animation-delay: 0.2s;
-    }
-
-    @keyframes bounce {
-        0% {
-            transform: translateY(0px);
-        }
-        30% {
-            transform: translateY(-6px);
-        }
-        100% {
-            transform: translateY(0px);
-        }
-    }
+  .typing {
+    display: inline-block;
+  }
+  .typing .typing-dot {
+    float: left;
+    width: 6px;
+    height: 6px;
+    border-radius: 50%;
+    margin: 2px;
+    background: #929292;
+    animation-name: bounce;
+    animation-duration: 0.8s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
   }
 
-  .blip-chat-state-metadata {
-    margin-top: -20px;
-    padding: 0 10px 10px 0;
+  .typing .typing-dot:nth-child(2) {
+    animation-delay: 0.1s;
   }
+  .typing .typing-dot:nth-child(3) {
+    animation-delay: 0.2s;
+  }
+
+  @keyframes bounce {
+    0% {
+      transform: translateY(0px);
+    }
+    30% {
+      transform: translateY(-6px);
+    }
+    100% {
+      transform: translateY(0px);
+    }
+  }
+}
+
+.blip-chat-state-metadata {
+  margin-top: -20px;
+  padding: 0 10px 10px 0;
+}
 </style>
