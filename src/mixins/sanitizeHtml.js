b924e2577a7c5e60bf217b7e7108f8f4e1e41565
Albert Moreira <albertm@take.net>
2019-04-26 17:23:16 -0300

    fix(Sanitize): Add tag u to allowed

--- src/mixins/sanitizeHtml.js
+++ src/mixins/sanitizeHtml.js
@@ -1,6 +1,6 @@
 import * as sanitize from 'sanitize-html'
 
-const mixin = input => sanitize(input, { allowedTags: sanitize.defaults.allowedTags.filter(tag => tag !== 'iframe') })
+const mixin = input => sanitize(input, { allowedTags: sanitize.defaults.allowedTags.filter(tag => tag !== 'iframe').concat(['u']) })
 
 export default {
   mixin
