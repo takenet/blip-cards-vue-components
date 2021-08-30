3f8292aacd13d8289e1e8c0bdf57abccea1adfff
michael haruo nakashima <mhnakashima@gmail.com>
2021-08-30 15:55:03 -0300

    bug(blip-cards): fixing loading gif

--- src/components/MediaLink/Audio.vue
+++ src/components/MediaLink/Audio.vue
@@ -5,50 +5,60 @@
         <img :src="trashSvg" />
       </div>
       <div v-if="editable && !isEditing" class="editIco" @click="toggleEdit">
-        <img :src="editSvg" />
+        <img class="loadinIcon" :src="editSvg" />
       </div>
       <div class="audio-player-wrapper" v-if="!isEditing">
-        <div v-if="!isLoading" class="audio-player-controls">
-          <span v-if="isPlaying" @click="togglePlay">
-            <svg
-              class="audio-player-button"
-              width="12px"
-              height="18px"
-              viewBox="0 0 12 18"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-            >
-              <g id="Pause" stroke="none" stroke-width="1" fill-rule="nonzero">
-                <path
-                  d="M4.61538462,17.351355 C4.61538462,17.7095549 4.33986449,18 4,18 L0.615384615,18 C0.275520128,18 0,17.7095885 0,17.351355 L0,0.648645015 C0,0.290411481 0.275520128,0 0.615384615,0 L4,0 C4.33986449,0 4.61538462,0.290411481 4.61538462,0.648645015 L4.61538462,17.351355 Z"
-                  id="Shape"
-                ></path>
-                <path
-                  d="M12,17.351355 C12,17.7095549 11.7244799,18 11.3846154,18 L8,18 C7.66013551,18 7.38461538,17.7095885 7.38461538,17.351355 L7.38461538,0.648645015 C7.38461538,0.290411481 7.66013551,0 8,0 L11.3846154,0 C11.7244799,0 12,0.290411481 12,0.648645015 L12,17.351355 Z"
-                  id="Shape"
-                ></path>
-              </g>
-            </svg>
-          </span>
-          <span v-else @click="togglePlay">
-            <svg
-              class="audio-player-button"
-              width="14px"
-              height="18px"
-              viewBox="0 0 14 18"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-            >
-              <g id="Play" stroke="none" stroke-width="1" fill-rule="evenodd">
-                <path
-                  d="M13.3805199,7.82862117 C14.2064934,8.35374536 14.2064934,9.64760806 13.3805199,10.1700254 L1.21459333,17.8790651 C0.677273234,18.2201251 0,17.7992137 0,17.1238607 L0,0.876139285 C0,0.200786266 0.677273234,-0.220125134 1.21459333,0.120934907 L13.3805199,7.82862117 Z"
-                  id="play-copy-3"
-                ></path>
-              </g>
-            </svg>
-          </span>
+        <div class="audio-player-controls">
+          <div v-if="!isLoading">
+            <span v-if="isPlaying" @click="togglePlay">
+              <svg
+                class="audio-player-button"
+                width="12px"
+                height="18px"
+                viewBox="0 0 12 18"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+              >
+                <g
+                  id="Pause"
+                  stroke="none"
+                  stroke-width="1"
+                  fill-rule="nonzero"
+                >
+                  <path
+                    d="M4.61538462,17.351355 C4.61538462,17.7095549 4.33986449,18 4,18 L0.615384615,18 C0.275520128,18 0,17.7095885 0,17.351355 L0,0.648645015 C0,0.290411481 0.275520128,0 0.615384615,0 L4,0 C4.33986449,0 4.61538462,0.290411481 4.61538462,0.648645015 L4.61538462,17.351355 Z"
+                    id="Shape"
+                  ></path>
+                  <path
+                    d="M12,17.351355 C12,17.7095549 11.7244799,18 11.3846154,18 L8,18 C7.66013551,18 7.38461538,17.7095885 7.38461538,17.351355 L7.38461538,0.648645015 C7.38461538,0.290411481 7.66013551,0 8,0 L11.3846154,0 C11.7244799,0 12,0.290411481 12,0.648645015 L12,17.351355 Z"
+                    id="Shape"
+                  ></path>
+                </g>
+              </svg>
+            </span>
+            <span v-else @click="togglePlay">
+              <svg
+                class="audio-player-button"
+                width="14px"
+                height="18px"
+                viewBox="0 0 14 18"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+              >
+                <g id="Play" stroke="none" stroke-width="1" fill-rule="evenodd">
+                  <path
+                    d="M13.3805199,7.82862117 C14.2064934,8.35374536 14.2064934,9.64760806 13.3805199,10.1700254 L1.21459333,17.8790651 C0.677273234,18.2201251 0,17.7992137 0,17.1238607 L0,0.876139285 C0,0.200786266 0.677273234,-0.220125134 1.21459333,0.120934907 L13.3805199,7.82862117 Z"
+                    id="play-copy-3"
+                  ></path>
+                </g>
+              </svg>
+            </span>
+          </div>
+          <div v-else>
+            <img class="audio-player-loading" :src="loadingGif" />
+          </div>
           <div class="audio-player-bar">
             <div class="slider" data-direction="horizontal" ref="audioSlider">
               <div class="progress" ref="audioProgress">
@@ -68,9 +78,6 @@
               <span>{{ getTimeFromSeconds(this.totalTime) }}</span>
             </div>
           </div>
-        </div>
-        <div v-else>
-          <img :src="loadingGif" />
         </div>
       </div>
       <div class="form" v-else>
@@ -159,7 +166,11 @@ export default {
     initAudio: function (uri) {
       this.audio = new Audio(uri)
       this.isLoading = true
-      this.audio.addEventListener('canplaythrough', this.audioReadyToPlay, false)
+      this.audio.addEventListener(
+        'canplaythrough',
+        this.audioReadyToPlay,
+        false
+      )
       this.audio.addEventListener('timeupdate', this.audioTimeUpdated)
       this.audio.addEventListener('loadedmetadata', this.audioLoaded)
       this.audio.addEventListener('ended', this.resetPlay)
@@ -297,7 +308,7 @@ export default {
       display: flex;
       flex-direction: column;
       flex-grow: 1;
-      margin-top: 8px;
+      margin-top: 6px;
     }
 
     .audio-player-time {
@@ -318,6 +329,17 @@ export default {
       height: 16px;
       cursor: pointer;
     }
+
+    .audio-loading {
+      padding-right: 12px;
+    }
+    .audio-player-loading {
+      flex-grow: 0;
+      width: 20px;
+      height: 20px;
+      margin-right: 12px;
+    }
+
     .slider {
       border-radius: 1.5px;
       height: 3px;
