09e6b34837fa60a07e665384c358e9543909c085
michael haruo nakashima <mhnakashima@gmail.com>
2021-09-01 09:35:59 -0300

    fixing loading icon

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -53,11 +53,18 @@ module.exports = {
         include: [resolve('src'), resolve('test')]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        test: /\.(png|jpe?g|svg)(\?.*)?$/i,
         loader: 'url-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        loader: 'url-loader',
+        options: {
+          encoding: 'base64'
         }
       },
       {
