88458f1b2e0d33cf3c4b619e172e050b1c815d81
Guilherme Alves de Oliveira <guilherme.alves@take.net>
2023-05-29 10:05:37 -0300

    fix: change size button to plainText

--- src/components/PlainText.vue
+++ src/components/PlainText.vue
@@ -10,7 +10,7 @@
           class="editIco trashIco icon-button-margin icon-button-top"
           icon="trash"
           variant="delete"
-          size="standard"
+          size="short"
           v-on:click="trash(document)"
         ></bds-button-icon>
         <bds-button-icon v-if="editable"
