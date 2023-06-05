44f4cdb0716102cf466f7f1ee0c9a7ebaf894238
mateus.ricciardi@take.net <mateus.ricciardi@take.net>
2023-06-05 12:12:04 -0300

    Creatingreplied base component

---
+++ src/components/ReplyCard/Replied/RepliedBase.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <replied-with-text 
+            v-if="replied.type === 'text/plain'"
+            :text="repliedValue"
+        />
+    </div>
+</template>
+  
+  <script>
+  import { default as base } from '../../../mixins/baseComponent.js'
+  
+  export default {
+    name: 'replied-base',
+    mixins: [base],
+    props: {
+      replied: {
+        type: Object,
+        default: {}
+      }
+    },
+    computed: {
+      repliedValue() {
+        return this.replied.value
+      }
+    },
+    created() {
+      console.log(this.replied)
+    },
+    methods: {
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  @import '../../../styles/variables.scss';
+  </style>
+  
