e2e9adb85cf0b055275432be84351b8fa7c648f6
mateus.ricciardi@take.net <mateus.ricciardi@take.net>
2023-06-05 12:11:41 -0300

    Creating replied with text component

---
+++ src/components/ReplyCard/Replied/RepliedWithText.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <bds-typo tag="span" variant="fs-16" margin="false">{{ repliedText }}</bds-typo>
+    </div>
+</template>
+  
+  <script>
+  import { default as base } from '../../../mixins/baseComponent.js'
+  
+  export default {
+    name: 'replied-with-text',
+    mixins: [base],
+    props: {
+      text: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      repliedText() {
+        return this.text
+      }
+    },
+    created() {
+      console.log(this.document)
+    },
+    methods: {
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  @import '../../../styles/variables.scss';
+  </style>
+  
