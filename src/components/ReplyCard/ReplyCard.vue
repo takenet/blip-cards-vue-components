fbc7be4cad94390329a5b18343d8f3fcdfcb5376
mateus.ricciardi@take.net <mateus.ricciardi@take.net>
2023-06-05 12:12:30 -0300

    Calling new reply components on reply card

--- src/components/ReplyCard/ReplyCard.vue
+++ src/components/ReplyCard/ReplyCard.vue
@@ -4,19 +4,10 @@
       <div class="reply-container">
         <div class="reply-header">
           <bds-icon name="undo" theme="outline" aria-label="Reply icon"></bds-icon>
-          <bds-typo tag="p" variant="fs-14" bold="regular" italic="true">Resposta</bds-typo>
+          <bds-typo tag="p" variant="fs-14" bold="regular" italic="true">{{ translations.reply || 'Resposta' }}</bds-typo>
         </div>
-
-        <div class="message-replied-container">
-          <span class="message-replied-bar" :class="{ 'own-message': isOwnMessage }"></span>
-          <div class="message-replied">
-            <bds-typo tag="p" variant="fs-16" bold="regular" margin="false" class="message-replied-text">{{ messageRepliedText }}</bds-typo>
-          </div>
-        </div>
-
-        <div>
-          <bds-typo tag="span" variant="fs-16" margin="false">{{ reply }}</bds-typo>
-        </div>
+        <in-reply-to-base :inReplyTo="inReplyTo" :isOwnMessage="isOwnMessage" />
+        <replied-base :replied="replied" />
       </div>
     </div>
   </div>
@@ -28,15 +19,21 @@ import { default as base } from '../../mixins/baseComponent.js'
 export default {
   name: 'reply-card',
   mixins: [base],
+  props: {
+    translations: {
+      type: Object,
+      default: () => ({})
+    }
+  },
   computed: {
     isOwnMessage() {
       return this.fullDocument.direction === this.fullDocument.content.inReplyTo.direction
     },
-    reply() {
-      return this.document.replied.value
+    replied() {
+      return this.document.replied
     },
-    messageRepliedText() {
-      return this.document.inReplyTo.value
+    inReplyTo() {
+      return this.document.inReplyTo
     }
   },
   created() {
@@ -59,41 +56,5 @@ export default {
   display: flex;
   align-items: center;
   gap: 0.5rem;
-}
-
-.message-replied-bar {
-  flex: none;
-  width: 4px;
-  border-top-left-radius: 8px;
-  border-bottom-left-radius: 8px;
-  background-color: $color-primary;
-
-
-  &.own-message {
-    background-color: $color-content-ghost;
-  }
-}
-
-.message-replied-container {
-  display: flex;
-  overflow: hidden;
-  background-color: #E0E0E0;
-  border: 1px solid #949494;
-  border-radius: 0.5rem;
-  margin: 1rem 0;
-}
-
-.message-replied {
-  padding: 0.5rem;
-}
-
-.message-replied-text {
-  display: -webkit-box;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  margin: 0;
-  text-align: left;
 }
 </style>
