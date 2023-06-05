f941035290e0526400e3ca5b6af251bb602e63ab
mateus.ricciardi@take.net <mateus.ricciardi@take.net>
2023-06-05 12:11:30 -0300

    Creating in reply to text component

---
+++ src/components/ReplyCard/InReplyTo/InReplyToText.vue
@@ -0,0 +1,43 @@
+<template>
+  <bds-typo tag="p" variant="fs-16" bold="regular" margin="false" class="message-replied-text">{{ inReplyToText }}</bds-typo>
+</template>
+  
+  <script>
+  import { default as base } from '../../../mixins/baseComponent.js'
+  
+  export default {
+    name: 'in-reply-to-text',
+    mixins: [base],
+    props: {
+      text: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      inReplyToText() {
+        return this.text
+      }
+    },
+    created() {
+      console.log(this.text)
+    },
+    methods: {
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  @import '../../../styles/variables.scss';
+
+  .message-replied-text {
+  display: -webkit-box;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  margin: 0;
+  text-align: left;
+}
+  </style>
+  
