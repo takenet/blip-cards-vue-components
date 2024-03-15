435ef246fc6bed0768fb13ad54aa9a0e6ba426ae
Andre Rossi <andre.rossi@take.net>
2024-03-15 15:56:38 -0300

    feat: created new calls card

---
+++ src/components/BlipCallsVoiceRequest.vue
@@ -0,0 +1,67 @@
+<template>
+  <div :class="'blip-container ' + position">
+    <div :class="'bubble body ' + position">
+      <bds-typo
+        class="typo"
+        variant="fs-16"
+        bold="regular"
+        >{{ bodyText }}
+      </bds-typo>
+    <div class="action">
+      <bds-grid margin="y-1" direction="row" justify-content="center" padding="x-2">
+        <bds-icon 
+          name="voip" 
+          color="white"/>
+        <bds-typo 
+          class="typo title-action" 
+          variant="fs-16" 
+          bold="regular">{{ actionText }}
+        </bds-typo>
+      </bds-grid>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'blip-calls-voice-request',
+  props: {
+    position: {
+      type: String,
+      default: 'right'
+    },
+    bodyText: {
+      type: String,
+    },
+    actionText: {
+      type: String,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../styles/variables.scss';
+
+.title-action {
+  margin-left: 8px;
+}
+
+.bubble {
+  max-width: 368px !important;
+}
+
+.action {
+  text-align: center;
+  border-top: 1px solid $color-content-ghost;
+  margin-top: 10px;
+  padding-top: 12px;
+}
+
+.body {
+  text-align: left;
+  overflow: hidden;
+  padding: 10px 20px;
+}
+</style>
