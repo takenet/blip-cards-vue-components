5ee56b0e59f42ac697c425ff3f0065491f6098e8
mateus.ricciardi@blip.ai <mateus.ricciardi@blip.ai>
2024-05-23 10:09:59 -0300

    Adding blip card date and external message to weblink

--- src/components/BlipCard.vue
+++ src/components/BlipCard.vue
@@ -195,6 +195,8 @@
           :deletable="deletable"
           :editing="isCardEditing"
           :on-cancel="cancel"
+          :is-external-message="checkIsExternalMessage(document)"
+          :external-message-text="translations.externalMessageText"
         />
 
         <survey
