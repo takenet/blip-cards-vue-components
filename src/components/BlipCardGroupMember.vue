8e4f1ce0665e7b2c1eefcae3ab28199e7fecaf7c
Vinicius Cesar <150076073+ViniBlip@users.noreply.github.com>
2024-08-14 15:11:43 -0300

    create initial group member component

---
+++ src/components/BlipCardGroupMember.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex" :class="`${isGroup ? 'group-' : ''}notification ` + position">
+    {{ groupMember }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'blip-card-group-member',
+  props: {
+    position: {
+      type: String,
+      default: 'left'
+    },
+    groupMember: {
+      type: String
+    },
+    isGroup: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+.blip-message-group .blip-card-group .group-notification.left,
+.group-notification.right {
+  float: none !important;
+}
+
+.blip-message-group .blip-card-group .group-notification.left {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.blip-message-group .blip-card-group .group-notification.right {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
