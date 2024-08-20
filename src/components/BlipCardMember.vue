36a13f28c8aaa8b048a762603376aa646a3a398f
Vinicius Cesar <150076073+ViniBlip@users.noreply.github.com>
2024-08-20 09:57:26 -0300

    fix(BlipCards): fix css alignment

--- src/components/BlipCardMember.vue
+++ src/components/BlipCardMember.vue
@@ -25,17 +25,19 @@ export default {
 
 
 <style scoped lang="scss">
-.blip-message-group .blip-card-group .group-notification.left,
-.group-notification.right {
+.blip-message-group .blip-card-group .group-notification.left, .group-notification.right, 
+.blip-card .notification.left, .notification.right {
   float: none !important;
 }
 
-.blip-message-group .blip-card-group .group-notification.left {
+.blip-message-group .blip-card-group .group-notification.left,
+.blip-card .notification.left {
   display: flex;
   justify-content: flex-start;
 }
 
-.blip-message-group .blip-card-group .group-notification.right {
+.blip-message-group .blip-card-group .group-notification.right,
+.blip-card .notification.right {
   display: flex;
   justify-content: flex-end;
 }
