2f46da5cab2229d392a49b2657a206373c0c2b8f
Marco Antonio de Carvalho Ramos <marco.ramos@blip.ai>
2024-10-09 17:56:43 -0300

    Fix code indentation

--- src/components/ReplyCard/InReplyTo/InReplyToImage.vue
+++ src/components/ReplyCard/InReplyTo/InReplyToImage.vue
@@ -1,46 +1,43 @@
 <template>
-    <bds-grid container direction="row" gap="3" justifyContent="space-between" align-items="center" >
-     
-      <bds-grid container direction="row" gap="1" justifyContent="space-between" align-items="flex-start" class="container-video">
-        <bds-icon theme="outline" name="file-image" size="large" />        
-
-        <bds-grid direction="column"  style="margin-top: -23px;">
-            <bds-typo margin="false">
-            {{ this.inReplyTo.value.title }}      
-          </bds-typo>
-          <bds-typo margin="false" class="text-in-reply" v-if="this.inReplyTo.value.text">
-            {{ this.inReplyTo.value.text }}  
-          </bds-typo>
-        </bds-grid>
+  <bds-grid container direction="row" gap="3" justifyContent="space-between" align-items="center" >
+    <bds-grid container direction="row" gap="1" justifyContent="space-between" align-items="flex-start" class="container-video">
+      <bds-icon theme="outline" name="file-image" size="large" />        
+      <bds-grid direction="column" style="margin-top: -23px;">
+        <bds-typo margin="false">
+          {{ this.inReplyTo.value.title }}      
+        </bds-typo>
+        <bds-typo margin="false" class="text-in-reply" v-if="this.inReplyTo.value.text">
+          {{ this.inReplyTo.value.text }}  
+        </bds-typo>
+      </bds-grid>
     </bds-grid>
-
-      <img class="image-replied" :src="inReplyTo.value.uri"/>
-    </bds-grid>
+    <img class="image-replied" :src="inReplyTo.value.uri"/>
+  </bds-grid>
 </template>
   
 <script>
-  export default {
-    name: 'in-reply-to-image',
-    mixins: [],
-    props: {
-      inReplyTo: {
-        type: Object,
-        default: () => ({})
-      }
+export default {
+  name: 'in-reply-to-image',
+  mixins: [],
+  props: {
+    inReplyTo: {
+      type: Object,
+      default: () => ({})
     }
   }
+}
 </script>
 
 <style scoped>
-  .image-replied {
-    max-width: 150px;
-  }
+.image-replied {
+  max-width: 150px;
+}
 
-  .text-in-reply {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    max-height: 30px;
-    width: 100%;
-  }
+.text-in-reply {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-height: 30px;
+  width: 100%;
+}
 </style>
