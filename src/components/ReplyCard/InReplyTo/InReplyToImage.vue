6bbca5b31b57458c4976a9a8e9a1cb4a30b3bcfa
Marco Antonio de Carvalho Ramos <marco.ramos@blip.ai>
2024-10-09 17:58:11 -0300

    Fix code spaces

--- src/components/ReplyCard/InReplyTo/InReplyToImage.vue
+++ src/components/ReplyCard/InReplyTo/InReplyToImage.vue
@@ -1,20 +1,20 @@
 <template>
-  <bds-grid container direction="row" gap="3" justifyContent="space-between" align-items="center" >
+  <bds-grid container direction="row" gap="3" justifyContent="space-between" align-items="center">
     <bds-grid container direction="row" gap="1" justifyContent="space-between" align-items="flex-start" class="container-video">
-      <bds-icon theme="outline" name="file-image" size="large" />        
+      <bds-icon theme="outline" name="file-image" size="large" />
       <bds-grid direction="column" style="margin-top: -23px;">
         <bds-typo margin="false">
-          {{ this.inReplyTo.value.title }}      
+          {{ this.inReplyTo.value.title }}
         </bds-typo>
         <bds-typo margin="false" class="text-in-reply" v-if="this.inReplyTo.value.text">
-          {{ this.inReplyTo.value.text }}  
+          {{ this.inReplyTo.value.text }}
         </bds-typo>
       </bds-grid>
     </bds-grid>
-    <img class="image-replied" :src="inReplyTo.value.uri"/>
+    <img class="image-replied" :src="inReplyTo.value.uri" />
   </bds-grid>
 </template>
-  
+
 <script>
 export default {
   name: 'in-reply-to-image',
