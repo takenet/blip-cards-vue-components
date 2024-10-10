32632c43cdba3bfdace268db1fbfc6d0afd12ae5
Marco Antonio de Carvalho Ramos <marco.ramos@blip.ai>
2024-10-09 23:32:19 -0300

    Add "right bubble" css class

--- src/components/ReplyCard/InReplyTo/InReplyToUnsupportedContent.vue
+++ src/components/ReplyCard/InReplyTo/InReplyToUnsupportedContent.vue
@@ -1,10 +1,10 @@
 <template>
   <bds-grid direction="row" padding="y-2" margin="x-1" align-items="center" gap="1">
     <bds-grid>
-      <bds-icon name="warning" theme="outline" size="medium"></bds-icon>
+      <bds-icon name="warning" theme="outline" size="medium" class="typo" />
     </bds-grid>
     <bds-grid>
-      <bds-typo variant="fs-14" bold="regular">{{ translations.unsupportedContent }}</bds-typo>
+      <bds-typo variant="fs-14" bold="regular" class="typo">{{ translations.unsupportedContent }}</bds-typo>
     </bds-grid>
   </bds-grid>
 </template>
