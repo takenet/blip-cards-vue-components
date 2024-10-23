f01043201b1f631e2e048e740d797939cc82ee02
Marco Antonio de Carvalho Ramos <marco.ramos@blip.ai>
2024-10-23 15:30:17 -0300

    Add new constant

--- src/utils/MessageTypesConstants.js
+++ src/utils/MessageTypesConstants.js
@@ -2,5 +2,6 @@ export const MessageTypesConstants = {
   THREAD_SUMMARY: 'application/vnd.iris.desk.thread-summary+json',
   MEDIALINK_MESSAGE: 'application/vnd.lime.media-link+json',
   REDIRECT_MESSAGE: 'application/vnd.lime.redirect+json',
-  TEXT_MESSAGE: 'text/plain'
+  TEXT_MESSAGE: 'text/plain',
+  LOCATION: 'application/vnd.lime.location+json'
 }
