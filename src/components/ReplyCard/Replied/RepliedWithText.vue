8cc602d93d037b5bc5e878b74321106106b95952
Marco Antonio de Carvalho Ramos <marco.ramos@blip.ai>
2024-10-23 15:31:44 -0300

    Remove extra text

--- src/components/ReplyCard/Replied/RepliedWithText.vue
+++ src/components/ReplyCard/Replied/RepliedWithText.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <bds-typo class="typo" tag="span" variant="fs-16" margin="false" v-html="sanitize(repliedText)"></bds-typo>
-    <bds-typo class="typo" tag="span" variant="fs-16" margin="false" v-html="sanitize(repliedText)"></bds-typo>
   </div>
 </template>
 
