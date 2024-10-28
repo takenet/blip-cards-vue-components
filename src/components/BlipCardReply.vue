6b738cb50d2555c3be50728abf61dc58a5f65755
Marco Antonio de Carvalho Ramos <marco.ramos@blip.ai>
2024-10-28 11:55:59 -0300

    Add new component

---
+++ src/components/BlipCardReply.vue
@@ -0,0 +1,54 @@
+<template>
+  <bds-button-icon
+    icon="redo"
+    variant="ghost"
+    size="short"
+    @click="replyMessage"
+    v-if="isButtonVisible"
+    class="blip-card-reply-button"
+  />
+</template>
+
+<script>
+export default {
+  name: 'blip-card-reply',
+  props: {
+    document: {
+      type: Object,
+      required: true
+    },
+    replyCallback: {
+      type: Function,
+      required: true
+    }
+  },
+  computed: {
+    isButtonVisible() {
+      return this.replyCallback !== undefined
+    }
+  },
+  methods: {
+    replyMessage() {
+      if (this.replyCallback) {
+        this.replyCallback(this.document)
+      } else {
+        throw new Error('Reply callback is not defined')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.blip-card {
+  .blip-card-reply-button {
+    display: none;
+  }
+
+  &:hover {
+    .blip-card-reply-button {
+      display: block;
+    }
+  }
+}
+</style>
