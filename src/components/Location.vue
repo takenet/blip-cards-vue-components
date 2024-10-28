1c9d8a5455dc55b299a66b31a5a529f1c124b5c6
Marco Antonio de Carvalho Ramos <marco.ramos@blip.ai>
2024-10-28 16:21:59 -0300

    Remove extra direction attribute

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="!isEditing" class="location" :class="isFailedMessage(status, position) + ' ' + getBlipContainer">
-    <bds-grid direction="row" justifyContent="space-between" gap="1" align-items="center" :direction="position === 'left' ? 'row' : 'row-reverse'">
+    <bds-grid justifyContent="space-between" gap="1" align-items="center" :direction="position === 'left' ? 'row' : 'row-reverse'">
       <div :class="simplified ? '' : 'bubble ' + position" :style=" simplified ? '' : 'width: ' + bubbleWidth">
         <bds-button-icon v-if="deletable"
           class="editIco trashIco icon-button-margin"
