138766b4920025074e43d3813bebe054107f0e64
Marco Antonio de Carvalho Ramos <marco.ramos@blip.ai>
2024-11-27 11:06:31 -0300

    Change string to constant

--- src/components/BlipCardReply.vue
+++ src/components/BlipCardReply.vue
@@ -15,6 +15,8 @@
 </template>
 
 <script>
+import { MessageTypesConstants } from '../utils/MessageTypesConstants'
+
 export default {
   name: 'blip-card-reply',
   props: {
@@ -42,52 +44,50 @@ export default {
 
       inReplyTo.id = this.document.id
       inReplyTo.metadata = this.document.metadata
+      inReplyTo.direction = this.document.direction
 
       if (this.replyCallback) {
         this.replyCallback({
           data: {
-            type: 'setIsReplying',
-            value: true,
-            inReplyTo,
-            direction: this.document.direction
+            show: true,
+            inReplyTo
           }
         })
       } else {
         throw new Error('Reply callback is not defined')
       }
     },
-
     formatMessage(message) {
+      const messageType = message.type || message.content.type
+      if (MessageTypesConstants.REPLY_MESSAGE === messageType) {
+        return this.formatMessage(message.content.replied)
+      }
+
       const formats = {
-        'text/plain': this.formatText,
-        'application/vnd.lime.media-link+json': this.formatMedia,
-        'application/vnd.lime.location+json': this.formatLocation,
-        'application/json': this.formatTemplate
+        [MessageTypesConstants.TEXT_MESSAGE]: this.formatText,
+        [MessageTypesConstants.MEDIALINK_MESSAGE]: this.formatMedia,
+        [MessageTypesConstants.LOCATION]: this.formatLocation,
+        [MessageTypesConstants.APPLICATION_JSON]: this.formatTemplate
       }
+      const format = formats[messageType]
 
-      let type = message.type || message.content.type
-
-      if (formats[type]) {
-        return formats[type](message)
+      if (format) {
+        return format(message)
       }
-
-      console.log('formatMessage', message)
 
       return {
-        type,
+        type: messageType,
         value: message.content
       }
     },
-
     formatTemplate(message) {
       let type = 'template'
 
       return {
-        type: 'application/json',
+        type: MessageTypesConstants.APPLICATION_JSON,
         value: {...message.content, type}
       }
     },
-
     formatImage(message) {
       return {
         type: 'image',
@@ -95,9 +95,18 @@ export default {
       }
     },
     formatText(message) {
+      let value
+      if (message.content && message.content.value) {
+        value = message.content.value
+      } else if (message.content) {
+        value = message.content
+      } else if (message.value) {
+        value = message.value
+      }
+
       return {
-        type: 'text/plain',
-        value: message.content.value || message.content
+        type: MessageTypesConstants.TEXT_MESSAGE,
+        value
       }
     },
     formatMedia(message) {
@@ -110,9 +119,9 @@ export default {
         value: message.content
       }
     },
-    formatLocation (message) {
+    formatLocation(message) {
       return {
-        type: 'application/vnd.lime.location+json',
+        type: MessageTypesConstants.LOCATION,
         value: message.content
       }
     }
